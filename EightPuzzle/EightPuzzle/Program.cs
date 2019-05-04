325723022072310dec504fcbb7e0e081ae97579d
shuvo <jayadul1@gmail.com>
2019-05-04 23:46:32 +0600

    puzzle

---
+++ EightPuzzle/EightPuzzle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightPuzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] puzzle =
+            {
+                1, 2, 4,
+                3, 0, 5,
+                7, 6, 8
+            };
+
+            Node root = new Node(puzzle);
+            UninformedSearch ui = new UninformedSearch();
+            List<Node> solution = ui.BreadthFirstSearch(root);
+
+            if (solution.Count > 0)
+            {
+                solution.Reverse();
+                for (int i = 0; i < solution.Count; i++)
+                    solution[i].PrintPuzzle();
+            }
+            else
+            {
+                Console.WriteLine("No Path to solution is found");
+            }
+            Console.Read();
+        }
+    }
+}
